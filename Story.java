276cba7706eb7563a0c1b11a25186cb36a413869
XXaNNeXX <anne_hensel@web.de>
2023-05-05 18:18:41 +0200

    Gauss' story in code

---
+++ Story.java
@@ -0,0 +1,30 @@
+public class Story {
+
+    //Aufsummieren aller Zahlen von 1 bis 100
+    static int Aufsummieren1(int upperLimit) {
+        int summe = 0;
+        for(int i=1;i<upperLimit;i++) {
+            summe += i;
+        }
+        return summe;
+    }
+
+    //Aufsummieren aller Zahlen von 100 bis 1
+    static int Aufsummieren2(int upperLimit) {
+        if(upperLimit == 1) {
+            return 1;
+        }
+        else {
+            return upperLimit + Aufsummieren2(upperLimit - 1);
+        }
+    }
+
+    //Aufsummieren aller Zahlen von 1 bis 100 und 100 bis 1
+    
+
+    public static void main(String[] args) {
+        System.out.println(Aufsummieren1(100));
+        System.out.println(Aufsummieren2(100));
+    }
+
+}
